d24726b18af5cafc3c2e0544c48677f790782f05
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-03-10 11:38:30 -0300

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\LinkController;
 
 /*
