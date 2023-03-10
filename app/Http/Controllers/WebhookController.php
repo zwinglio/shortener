4c7b9c5bc7dff3cf27abee6bf35a9b5174c8d58d
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-03-10 11:29:46 -0300

    github webhook

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    public function gitPull(Request $request)
+    {
+        $payload = json_decode($request->getContent(), true);
+        $branch = $payload['ref'];
+        $branch = str_replace('refs/heads/', '', $branch);
+
+        $secret = $payload['secret'];
+        if ($secret != 'qwe@1234') {
+            return response()->json(['error' => 'Invalid secret'], 401);
+        }
+
+        if ($branch == 'master') {
+            $outputPull = shell_exec('git pull');
+            $outputBuild = shell_exec('npm run build');
+            $outputRsync = shell_exec('rsync -av --progress --exclude "index.php" public/ ../public_html/');
+
+            return response()->json(['output' => [$outputPull, $outputBuild, $outputRsync]]);
+        }
+    }
+}
